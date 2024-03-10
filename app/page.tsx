99e867cd1355cc1cd02e214342e5b754adc5203d
Hm-source <swo8852@gmail.com>
2024-03-10 20:03:47 +0900

    design : main page 제목 변경

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center content-center p-16">
-      <div className="flex flex-col w-full items-center content-center">
-        <div className="font-bold w-full text-center text-2xl mb-8">MAIN PAGE</div>
-        <button>LOGOUT</button>
+      <div className="flex flex-row">
+        <div className="flex m-3 items-center align-center">
+          <Image src="./sound_icon.svg" alt="BeMusician_icon" width={300} height={300} />
+        </div>
+        <div className="flex flex-col w-full align-center m-2 justify-center">
+          <div className="font-bold text-4xl mb-5">Be</div>
+          <div className="font-bold text-4xl mb-5">Musician</div>
+        </div>
       </div>
     </main>
   )
