f6722eeedada0b8633097c41b1b108ef61b76ae6
Hm-source <swo8852@gmail.com>
2024-03-10 20:02:40 +0900

    design : nav-bar loop로 수정

--- components/Common/Header.tsx
+++ components/Common/Header.tsx
@@ -7,24 +7,20 @@ export default function Header() {
     <>
       <div className="flex">
         <div className="flex w-full flex-row justify-between items-center">
-          <a href="#" className="inline-block m-4 uppercase font-bold text-gray">
+          <a href="#" className="inline-block m-4 uppercase text-gray rounded-lg px-3 py-2 hover:bg-slate-100 hover:text-slate-900">
             <Image src="./home_icon.svg" alt="home" width={30} height={30} />
             <span className="sr-only">(current)</span>
           </a>
-          <div className="">
-            <ul className="flex">
-              <li className="m-4 flex flex-row">
-                <a href="/SignIn" className="uppercase font-bold text-gray underline underline-offset-1">
-                  Sign In
-                </a>
-              </li>
-              <li className="m-4 flex flex-row">
-                <a href="/SignUp" className="uppercase font-bold text-gray underline underline-offset-1">
-                  Sign Up
-                </a>
-              </li>
-            </ul>
-          </div>
+          <nav className="flex sm:justify-center space-x-4 m-4 ">
+            {[
+              ['Sign In', '/SignIn'],
+              ['Sign Up', '/SignUp']
+            ].map(([title, url]) => (
+              <a key={`${title}`} href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">
+                {title}
+              </a>
+            ))}
+          </nav>
         </div>
       </div>
     </>
