9ee15e16f05ef1a0bee48900996f227d5e10bade
Hm-source <swo8852@gmail.com>
2024-03-11 04:28:07 +0900

    feat: login page 추가 수정

---
+++ app/SignIn/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import Image from 'next/image'
+import SignInInput from '../../components/SignIn/SignInInput'
+import React from 'react'
+export default function signIn() {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center content-center p-16">
+      <div className="flex flex-row">
+        <div className="flex m-3 items-center align-center">
+          <Image src="./sound_icon.svg" alt="BeMusician_icon" width={300} height={300} />
+        </div>
+        <div className="flex flex-col w-full align-center m-2 justify-center">
+          <div className="font-bold text-4xl mb-5">Be</div>
+          <div className="font-bold text-4xl mb-5">Musician</div>
+        </div>
+      </div>
+      <div className="w-full">
+        <SignInInput />
+      </div>
+    </main>
+  )
+}
--- components/SignIn/SignInInput.tsx
+++ components/SignIn/SignInInput.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useSession, signIn, signOut } from 'next-auth/react'
+import Image from 'next/image'
 // import { useRouter } from 'next/router'
 export default function SignInInput() {
   const { data: session, status } = useSession()
@@ -19,22 +20,57 @@ export default function SignInInput() {
     }*/
   }
   if (status === 'loading') {
-    return <p>Hang on there...</p>
+    return <p className="text-center">...Loading</p>
   }
 
   if (status === 'authenticated') {
     return (
       <>
-        <p>Signed in as {userEmail}</p>
-        <button onClick={() => signOut()}>Sign out</button>
+        <div className="w-full">
+          <div className="flex flex-col w-full">
+            <div className="text-md text-center text-gray-500 m-3">Signed in as {userEmail}</div>
+
+            <div className="flex justify-center p-3 rounded-md m-3 bg-slate-400 text-slate-100 font-medium hover:bg-slate-300 hover:text-slate-900 text-center">
+              <button onClick={() => signOut()}> Logout</button>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
 
   return (
     <>
-      <p>Not signed in.</p>
-      <button onClick={() => onClickLoginRequest()}>Sign in</button>
+      <div className="w-full">
+        <div className="flex flex-col w-full">
+          <div className="flex items-center justify-center m-1">
+            <button className="m-1" onClick={() => onClickLoginRequest()}>
+              <Image src="/kakao_login.png" alt="kakao_login" width={183} height={45} />
+            </button>
+          </div>
+
+          <div className="text-md text-center text-gray-500 m-3">or use your account</div>
+          <div className="flex flex-row justify-center w-full">
+            <div className="flex flex-col w-1/2">
+              <div className="flex flex-row p-1 m-2 ">
+                <label className="text-xl w-1/4 flex justify-start items-center mr-2" htmlFor="email">
+                  email
+                </label>
+                <input className="w-3/4 flex items-center px-3 py-3 m-1" id="email" type="email" placeholder="email" />
+              </div>
+              <div className="flex flex-row p-1 m-2">
+                <label className="text-xl  w-1/4 flex justify-start items-center mr-2" htmlFor="password">
+                  password
+                </label>
+                <input className="w-3/4 flex items-center px-3 py-3 m-1" id="password" type="password" placeholder="password" />
+              </div>
+            </div>
+            <div className="flex justify-center p-3 rounded-md m-4 bg-slate-400 text-slate-100 font-medium hover:bg-slate-300 hover:text-slate-900 text-center">
+              <button onClick={() => signIn()}> Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
